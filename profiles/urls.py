acccca468282af21bac3f82b53c4675cb77923ab
Aponovi <elfi.krupsky@gmail.com>
2022-06-22 12:35:09 +0200

    dispatch urls and html files

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,7 +6,6 @@ from . import views
 
 app_name = 'profiles'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('', views.profiles_index, name='index'),
+    path('<str:username>/', views.profile, name='profile'),
 ]
