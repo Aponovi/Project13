32fbe2b81f28073d536e8b5a0e81f6bda6a8a8c1
Aponovi <elfi.krupsky@gmail.com>
2022-06-23 16:29:02 +0200

    Tests Lettings Views

--- lettings/tests.py
+++ lettings/tests.py
@@ -7,22 +7,50 @@ from .models import Letting, Address
 
 
 @pytest.mark.django_db(transaction=True, reset_sequences=True)
-def test_letting_index_view():
-    client = Client()
-    Address.objects.create(number=1007,
-                           street='Mountain Drive',
-                           city='Gotham City',
-                           state='NJ',
-                           zip_code='12345',
-                           country_iso_code='USA'
-                           )
-    Letting.objects.create(title="Wayne Manor",
-                           address_id=1)
-    path = reverse('lettings:index')
-    response = client.get(path)
-    content = response.content.decode()
-    expected_content = "<h1>Lettings</h1>"
+class Tests_Lettings_Views:
 
-    assert expected_content in content
-    assert response.status_code == 200
-    assertTemplateUsed(response, "lettings/index.html")
+    def test_lettings_index_view(self):
+        client = Client()
+        expected_content_1 = "<h1>Lettings</h1>"
+        expected_content_2 = "Wayne Manor"
+
+        Address.objects.create(number=1007,
+                               street='Mountain Drive',
+                               city='Gotham City',
+                               state='NJ',
+                               zip_code='12345',
+                               country_iso_code='USA'
+                               )
+        Letting.objects.create(title="Wayne Manor",
+                               address_id=1)
+        path = reverse('lettings:index')
+        response = client.get(path)
+        content = response.content.decode()
+
+        assert expected_content_1 in content
+        assert expected_content_2 in content
+        assert response.status_code == 200
+        assertTemplateUsed(response, "lettings/index.html")
+
+    def test_lettings_letting_view(self):
+        client = Client()
+        expected_content_1 = "<h1>Wayne Manor</h1>"
+        expected_content_2 = "<p>1007 Mountain Drive</p>"
+
+        Address.objects.create(number=1007,
+                               street='Mountain Drive',
+                               city='Gotham City',
+                               state='NJ',
+                               zip_code='12345',
+                               country_iso_code='USA'
+                               )
+        Letting.objects.create(title="Wayne Manor",
+                               address_id=1)
+        path = reverse('lettings:letting', kwargs={'letting_id': 1})
+        response = client.get(path)
+        content = response.content.decode()
+
+        assert expected_content_1 in content
+        assert expected_content_2 in content
+        assert response.status_code == 200
+        assertTemplateUsed(response, "lettings/letting.html")
