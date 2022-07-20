0e4e05844b30d937e88c9e74e6379208b67dfbdd
Aponovi <elfi.krupsky@gmail.com>
2022-07-20 14:37:52 +0200

    testing site and change name

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -19,13 +19,6 @@ ALLOWED_HOSTS = []
 if not DEBUG:
     ALLOWED_HOSTS += ['.herokuapp.com']
 
-# # https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
-# SESSION_COOKIE_SECURE = True
-# # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
-# CSRF_COOKIE_SECURE = True
-
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
